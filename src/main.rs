57e8e2cbcf304615d14bc07a55fc1f114d6d4b6e
xrenovozzz <100874385+xrenovozzz@users.noreply.github.com>
2024-12-01 18:50:03 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,54 @@
+//! Генератор изображения треугольника Серпинского
+//!
+//! Пример использования Rust для создания PNG-изображения
+
 extern crate image;
 extern crate rand;
 
 use rand::Rng;
-use std::fs::File;
-use std::path::Path;
 
-// Точка для построения треугольника
+/// Точки, используемые для построения треугольника и нанесения точек на холст
 pub struct Point {
     x: u32,
     y: u32,
 }
 
-const WIDTH: u32 = 800;
-const HEIGHT: u32 = 600;
+/// Ширина PNG-изображения
+pub const WIDTH:  u32 = 800;
+/// Высота PNG-изображения
+pub const HEIGHT: u32 = 600;
 
-fn main() {
-    // Создаем буфер изображения
+/// Основная программа
+pub fn main() {
     let mut img = image::ImageBuffer::from_fn(WIDTH, HEIGHT, |x, y| {
         if x == 0 && y == 0 {
-            image::Luma([0u8])
+            image::Luma([0u8]) // Чёрный цвет для начальной точки
         } else {
-            image::Luma([255u8])
+            image::Luma([255u8]) // Белый цвет для остальных точек
         }
     });
 
-    let mut cnt: u32 = 10_000;
+    let mut cnt: u32 = 1_000_000; // Количество итераций для построения точек
 
-    // Определяем точки треугольника
     let pts: [Point; 3] = [
-        Point { x: WIDTH / 2, y: 0 },
-        Point { x: 0, y: HEIGHT },
-        Point { x: WIDTH, y: HEIGHT },
+        Point {x: WIDTH / 2, y: 0},     // Верхняя вершина треугольника
+        Point {x: 0, y: HEIGHT},        // Левая нижняя вершина
+        Point {x: WIDTH, y: HEIGHT},    // Правая нижняя вершина
     ];
 
-    let mut num: usize;
-    let mut p = Point { x: 350, y: 350 };
-    let pixel = image::Luma([0u8]);
+    let mut p = Point { x: rand::thread_rng().gen_range(0..WIDTH),   // Случайная начальная точка
+                        y: rand::thread_rng().gen_range(0..HEIGHT),
+    };
 
-    // Генерация точек по методу хаотических треугольников
+    let pixel = img[(0, 0)]; // Цвет пикселя (изначально чёрный)
     while cnt > 0 {
-        cnt -= 1;
-        num = rand::thread_rng().gen_range(0..3);
-        p.x = (p.x + pts[num].x) / 2;
-        p.y = (p.y + pts[num].y) / 2;
-        img.put_pixel(p.x, p.y, pixel);
+        cnt = cnt - 1; // Уменьшение счётчика итераций
+        let num = rand::thread_rng().gen_range(0..3); // Случайный выбор вершины (0, 1 или 2)
+        p.x = (p.x + pts[num].x) / 2; // Новая x-координата — середина между текущей точкой и вершиной
+        p.y = (p.y + pts[num].y) / 2; // Новая y-координата — середина между текущей точкой и вершиной
+        img.put_pixel(p.x, p.y, pixel); // Установка пикселя на изображении
     }
 
-    // Сохраняем изображение
-    let ref mut fout = File::create(&Path::new("tri.png")).unwrap();
-    img.save(fout).unwrap();
-}
+    img.save("tri.png").unwrap(); // Сохранение изображения в файл "tri.png"
+} 
+

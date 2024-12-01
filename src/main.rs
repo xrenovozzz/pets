66a684f1d469eb4a27ce7df5ef30f842dbefc673
xrenovozzz <100874385+xrenovozzz@users.noreply.github.com>
2024-12-01 18:38:31 +0300

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,21 @@
-//! sierpinski triangle image generator
-//!
-//! an example of using rust to generate a png image file
-
 extern crate image;
 extern crate rand;
 
 use rand::Rng;
+use std::fs::File;
+use std::path::Path;
 
-/// points used to build the triangle and plot points on the canvas
+// Точка для построения треугольника
 pub struct Point {
     x: u32,
     y: u32,
 }
 
-/// width of the png image
-pub const WIDTH:  u32 = 800;
-/// height of the png image
-pub const HEIGHT: u32 = 600;
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 600;
 
-/// main program
-pub fn main() {
+fn main() {
+    // Создаем буфер изображения
     let mut img = image::ImageBuffer::from_fn(WIDTH, HEIGHT, |x, y| {
         if x == 0 && y == 0 {
             image::Luma([0u8])
@@ -28,26 +24,29 @@ pub fn main() {
         }
     });
 
-    let mut cnt: u32 = 1_000_000;
+    let mut cnt: u32 = 10_000;
 
+    // Определяем точки треугольника
     let pts: [Point; 3] = [
-        Point {x: WIDTH / 2, y: 0},
-        Point {x: 0, y: HEIGHT},
-        Point {x: WIDTH, y: HEIGHT},
+        Point { x: WIDTH / 2, y: 0 },
+        Point { x: 0, y: HEIGHT },
+        Point { x: WIDTH, y: HEIGHT },
     ];
 
-    let mut p = Point { x: rand::thread_rng().gen_range(0..WIDTH),
-                        y: rand::thread_rng().gen_range(0..HEIGHT),
-    };
+    let mut num: usize;
+    let mut p = Point { x: 350, y: 350 };
+    let pixel = image::Luma([0u8]);
 
-    let pixel = img[(0, 0)];
+    // Генерация точек по методу хаотических треугольников
     while cnt > 0 {
-        cnt = cnt - 1;
-        let num = rand::thread_rng().gen_range(0..3);
+        cnt -= 1;
+        num = rand::thread_rng().gen_range(0..3);
         p.x = (p.x + pts[num].x) / 2;
         p.y = (p.y + pts[num].y) / 2;
         img.put_pixel(p.x, p.y, pixel);
     }
 
-    img.save("tri.png").unwrap();
+    // Сохраняем изображение
+    let ref mut fout = File::create(&Path::new("tri.png")).unwrap();
+    img.save(fout).unwrap();
 }
